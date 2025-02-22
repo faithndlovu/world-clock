5fb59bf9078382c64131018abf661b2f5113456a
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-02-22 20:39:02 +0200

    updated js

--- Javascript/index.js
+++ Javascript/index.js
@@ -18,10 +18,18 @@ johannesburgTimeElement.innerHTML=`${johannesburgTime.format("h:mm:ss")}<small>$
 }
 
 function updateCity(event){
-    let cityTimeZone= event.target.value;
-    let cityTime =moment().tz(cityTimeZone);
-    let citiesElement= document.querySelector("#cities");
-    citiesElement.innerHTML= "Hello";
+    let cityTimeZone = event.target.value;
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+<div class="city">
+    <div>
+        <h2>${cityTimeZone}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small>
+</div>
+</div>`;
 }
 updateTime();
 setInterval(updateTime, 1000);
