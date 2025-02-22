2c30fe7461e19ed3ba836ad01e0d612147d174ad
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-02-22 17:49:39 +0200

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,12 +1,11 @@
-setInterval((function(){
+setInterval(function(){
 let capeTownElement=document.querySelector("#cape-town");
 let capeTownDateElement=capeTownElement.querySelector(".date");
 let capeTownTimeElement=capeTownElement.querySelector(".time");
 let capeTownTime= moment().tz(Africa/Cape_Town);
 
 capeTownDateElement.innerHTML=capeTownTime.format("MMMM Do YYYY");
-capeTownTimeElement.innerHTML=capeTownTime.format("h:mm:ss")}<small>capeTownTime.format("h:mm:ss)>[<small]A[</small>]"
-);
+capeTownTimeElement.innerHTML=capeTownTime.format("h:mm:ss")<small>capeTownTime.format("h:mm:ss)>[<small]A[</small>]");
 }, 1000);
 
 let johannesburgElement=document.querySelector("#johannesburg");
