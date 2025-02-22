2eff2880c7754781688e48f39abc71ee50ba0268
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-02-22 18:04:49 +0200

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,17 +1,20 @@
-setInterval(function(){
-let capeTownElement=document.querySelector("#cape-town");
-let capeTownDateElement=capeTownElement.querySelector(".date");
-let capeTownTimeElement=capeTownElement.querySelector(".time");
-let capeTownTime= moment().tz(Africa/Cape_Town);
+function updateTime(){
+let capeTownElement = document.querySelector("#cape-town");
+let capeTownDateElement = capeTownElement.querySelector(".date");
+let capeTownTimeElement = capeTownElement.querySelector(".time");
+let capeTownTime= moment().tz("Africa/Cape_Town");
 
-capeTownDateElement.innerHTML=capeTownTime.format("MMMM Do YYYY");
-capeTownTimeElement.innerHTML=capeTownTime.format("h:mm:ss")<small>capeTownTime.format("h:mm:ss)>[<small]A[</small>]");
-}, 1000);
+capeTownDateElement.innerHTML = capeTownTime.format("MMMM Do YYYY");
+capeTownTimeElement.innerHTML = capeTownTime.format("h:mm:ss [<small>]A[</small>]");
+
 
 let johannesburgElement=document.querySelector("#johannesburg");
 let johannesburgDateElement=johannesburgElement.querySelector(".date");
 let johannesburgTimeElement=johannesburgElement.querySelector(".time");
-let johannesburgTime= moment().tz(Africa/Johannesburg);
+let johannesburgTime= moment().tz("Africa/Johannesburg");
 
 johannesburgDateElement.innerHTML=johannesburgTime.format("MMMM Do YYYY");
 johannesburgTimeElement.innerHTML=`${johannesburgTime.format("h:mm:ss")}<small>${capeTownTime.format("A")}</small>`;
+}
+updateTime();
+setInterval(updateTime, 1000);
