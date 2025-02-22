fde54772e25eb7c0a414f24dc87e9ea36e76e3de
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-02-22 14:00:44 +0200

    updated html and added js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,9 +1,17 @@
-const moment = require("moment");
+let moment = require("moment");
 
 let capeTownElement=document.querySelector("#cape-town");
 let capeTownDateElement=capeTownElement.querySelector(".date");
 let capeTownTimeElement=capeTownElement.querySelector(".time");
-let capeTownTime= moment().tz(Africa/Cape-Town);
+let capeTownTime= moment().tz(Africa/Cape_Town);
 
 capeTownDateElement.innerHTML=capeTownTime.format("MMMM Do YYYY");
 capeTownTimeElement.innerHTML=`${capeTownTime.format("h:mm:ss")}<small>${capeTownTime.format("A")}</small>`;
+
+let johannesburgElement=document.querySelector("#johannesburg");
+let johannesburgDateElement=johannesburgElement.querySelector(".date");
+let johannesburgTimeElement=johannesburgElement.querySelector(".time");
+let johannesburgTime= moment().tz(Africa/Johannesburg);
+
+johannesburgDateElement.innerHTML=johannesburgTime.format("MMMM Do YYYY");
+johannesburgTimeElement.innerHTML=`${johannesburgTime.format("h:mm:ss")}<small>${capeTownTime.format("A")}</small>`;
